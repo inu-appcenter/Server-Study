52115ec0fb894b1cb899df1b01f3e97f2bf0369c
LimHeonJun <jumong40@naver.com>
2018-07-04 09:43:37 +0000

    1주차 스터디

---
+++ webserver.js
@@ -0,0 +1,11 @@
+const http = require('http');
+ 
+const hostname = '35.231.250.224';
+const port = 52273;
+ 
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
